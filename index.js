6f3ef98f9ed41460ee166c0236ccf6c9df2305ef
soharab007 <soharabhossain943@gmail.com>
2022-11-30 03:26:27 +0600

    assignment recheck

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ksaovkw.mongodb.net/?retryWrites=true&w=majority`
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 const verifyJwt = (req, res, next) => {
