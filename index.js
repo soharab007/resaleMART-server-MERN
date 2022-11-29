9180330ed5fe49ad33c6635661e092a9705cec0d
soharab007 <soharabhossain943@gmail.com>
2022-11-29 11:04:52 +0600

    git ignore added

--- index.js
+++ index.js
@@ -1,33 +1,262 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const { sign } = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-const app = express();
 
-
-// middleware
+// middleware ass
 app.use(cors());
 app.use(express.json());
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8lhtxek.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const verifyJwt = (req, res, next) => {
+    const autHorization = req.headers.authorization
+
+    if (!autHorization) {
+        res.status(401).send('Unauthenticated access denied')
+    };
+
+    const token = autHorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+        if (error) {
+            res.status(403).send('something wrong')
+        }
+        req.decoded = decoded;
+        next()
+    })
+};
+
 
 async function run() {
     try {
-        // const productsCollection = client.db("mirrawDb").collection("products");
-        const phonesCollection = client.db("usedPhone").collection("phones");
+        const productsCollection = client.db("resailMart").collection("Products");
+        const myProductsCollection = client.db("resailMart").collection("myProducts");
+        const bookingsCollection = client.db("resailMart").collection("bookings");
+        const categoryCollection = client.db("resailMart").collection("category");
+        const usersCollection = client.db("resailMart").collection("users");
+        const sellerProductsCollection = client.db("resailMart").collection("SellerProducts");
+        const advertisedProductsCollection = client.db("resailMart").collection("advertised");
 
-        app.get('/phones', async (req, res) => {
-            // const id = req.params.id;
+        //  
+        // const productsCollection = client.db("mirrawDb").collection("products");
+        // const bookingsCollection = client.db("mirrawDb").collection("bookings");
+        // const categoryCollection = client.db("mirrawDb").collection("category");
+        // const usersCollection = client.db("mirrawDb").collection("users");
+        // const sellerProductsCollection = client.db("mirrawDb").collection("SellerProducts");
+        // const advertisedProductsCollection = client.db("mirrawDb").collection("advertised");
+        // // 
+
+
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        });
+
+        app.get('/category', async (req, res) => {
             const query = {};
-            const phone = await phonesCollection.find(query).toArray();
-            res.send(phone)
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            let query = { categoryId: parseInt(id) };
+            // const results = await myProductsCollection.find(query).toArray();
+            const results = await productsCollection.find(query).toArray();
+            res.send(results);
+        });
+        // 
+        // the below code return the two products
+        // app.get('/myproducts', async (req, res) => {
+        //     const query = {};
+        //     const product = await myProductsCollection.find(query).toArray();
+        //     res.send(product)
+        // })
+        // 
+        app.put('/report/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    report: 'Reported'
+                },
+            };
+            const result = await productsCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.put('/reportseller/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    reportedSeller: 'ReportedSeller'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+
+        });
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const results = await bookingsCollection.find(query).toArray();
+            res.send(results);
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingsCollection.deleteOne(query)
+            res.send(result);
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result);
+        });
+        // from doc portal
+        // app.get('/bookings', verifyJWT, async (req, res) => {
+        //     const email = req.query.email;
+        //     const decodedEmail = req.decoded.email;
+
+        //     if (email !== decodedEmail) {
+        //         return res.status(403).send({ message: 'forbidden access' });
+        //     }
+
+        //     const query = { email: email };
+        //     const bookings = await bookingsCollection.find(query).toArray();
+        //     res.send(bookings);
+        // });
+        // 
+        // app.get('/users', verifyJwt, async (req, res) => {
+        //     const email = req.query.email;
+        //     const decodedEmail = req.decoded.email;
+        //     if (email !== decodedEmail) {
+        //         // if (decodedEmail !== req.query.email) {
+        //         res.status(401).send('theft access');
+        //     };
+        //     const query = { email: email };
+        //     // const query = { email: decodedEmail };
+        //     const results = await usersCollection.findOne(query);
+        //     res.send(results);
+        // });
+
+        // comment for try below
+        app.get('/users', verifyJwt, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            if (decodedEmail !== req.query.email) {
+                res.status(401).send('theft access');
+            };
+            const query = { email: decodedEmail };
+            const results = await usersCollection.findOne(query);
+            res.send(results);
+        });
+        // 
+
+        app.post('/myProducts', async (req, res) => {
+            const ProductInfo = req.body;
+            const result = await sellerProductsCollection.insertOne(ProductInfo);
+            res.send(result);
+        })
+
+
+        app.get('/myProducts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const results = await sellerProductsCollection.find(query).toArray();
+            res.send(results);
+        });
+
+        app.delete('/myProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await sellerProductsCollection.deleteOne(query)
+            res.send(result);
 
         })
+
+
+        app.post('/advertised', async (req, res) => {
+            const Product = req.body;
+            const result = await advertisedProductsCollection.insertOne(Product);
+            res.send(result);
+        });
+
+        app.get('/advertised', async (req, res) => {
+            const query = {};
+            const result = await advertisedProductsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/seller', async (req, res) => {
+            const query = { userType: "Buyer" };
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.delete('/seller/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+
+        })
+
+
+        app.get('/buyer', async (req, res) => {
+            const query = { userType: "Seller" };
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.delete('/buyer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query)
+            res.send(result);
+        });
+
+        app.get('/reportedProducts', async (req, res) => {
+            const query = { report: 'Reported' };
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+
+        });
+
+        app.delete('/reportedProducts/id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
     }
     finally {
@@ -36,9 +265,22 @@ async function run() {
 }
 run().catch(error => console.log(error));
 
-app.get('/', async (req, res) => {
-    res.send('assignment-12-server is running');
-});
-app.listen(port, () =>
-    console.log(`assignment-12-server is running on ${port}`)
-)
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`My Server is running on port ${port}`)
+})
